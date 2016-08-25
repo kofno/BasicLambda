55e2e1268c4352fee02344582d8e5a36b19cda7c
cwmyers <chris@chrisandjo.com>
2016-08-25 21:13:06 +1000

    Brings project inline with current language (#1)

--- src/BasicData.purs
+++ src/BasicData.purs
@@ -1,9 +1,8 @@
 module BasicData where
 
-import Prelude
+import Prelude (class Show, pure, bind, ($), (<>))
 
-import Data.Foreign
-import Data.Foreign.Class
+import Data.Foreign.Class (class IsForeign, readProp)
 
 data LambdaData = LambdaData { key1 :: String
                              , key2 :: String
@@ -11,11 +10,10 @@ data LambdaData = LambdaData { key1 :: String
 
 instance showLambdaData :: Show LambdaData where
   show (LambdaData o) =
-    "LambdaData { key1: " ++ o.key1 ++ ", key2: " ++ o.key2 ++ " }"
+    "LambdaData { key1: " <> o.key1 <> ", key2: " <> o.key2 <> " }"
 
 instance lambdaDataIsForeign :: IsForeign LambdaData where
   read value = do
     k1 <- readProp "key1" value
     k2 <- readProp "key2" value
-    return $ LambdaData { key1: k1, key2: k2 }
-
+    pure $ LambdaData { key1: k1, key2: k2 }
